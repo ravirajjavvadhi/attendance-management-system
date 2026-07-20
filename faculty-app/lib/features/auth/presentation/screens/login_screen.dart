1f1bd6f2031370016fd995b30df1e38c9e31169b
ravirajjavvadhi <ravirajjavvadhi@gmail.com>
2026-07-20 06:48:01 +0530

    build: compile and update parent, faculty, and student APKs

--- faculty-app/lib/features/auth/presentation/screens/login_screen.dart
+++ faculty-app/lib/features/auth/presentation/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:eduflow_core/eduflow_core.dart';
+import 'package:dio/dio.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
